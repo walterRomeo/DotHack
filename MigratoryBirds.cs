636e2156d488bb01b21d7fddb9d471aaf8b453ec
Walter Rodas <walter.r.rodas@gmail.com>
2021-05-21 17:24:34 +1000

    Migratory birds solution added

---
+++ MigratoryBirds.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MigratoryBirds
+{
+
+    class Program
+    {
+        static int migratoryBirds(List<int> arr)
+        {
+            int count = 0;
+            Dictionary<int,int> birdId = new Dictionary<int, int>();
+            foreach(int value in arr)
+            {
+                if(birdId.ContainsKey(value) == false)
+                {
+                    birdId.Add(value,0);
+                }
+                else{
+                    birdId[value]++;
+                }
+            }
+            count = birdId.OrderBy(x =>x.Key).FirstOrDefault( bird => bird.Value == birdId.Values.Max()).Key;
+            //Console.WriteLine(String.Join(" ",birdId.Select( val => Convert.ToString(val.Value)) ));
+         return count;   
+        }
+        static void Main(string[] args)
+        {
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+            int result = migratoryBirds(arr);
+            Console.WriteLine(result);
+        }
+    }
+}
